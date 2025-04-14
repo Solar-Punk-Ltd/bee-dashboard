854d0a284074fb54d772949355b2e865ee19ba53
Peter Ott <peter.ott@solarpunk.buzz>
2025-04-14 13:12:26 +0200

    Adds SwarmIcons as component

---
+++ src/components/FilesHandler/FileUpload/SwarmIcon.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from 'react'
+import icon from '../../../assets/images/swarmIcon.png'
+
+export const SwarmIcon = (): ReactElement => {
+  return <img src={icon} alt="Orange Swarm icon" height="16" />
+}
