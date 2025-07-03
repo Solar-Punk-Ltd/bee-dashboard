7c515c6cd935b060f4593138db6c5558d7dc40c2
Roland Seres <roland.seres90@gmail.com>
2025-07-03 11:10:15 +0200

    refactor: owner status bar

--- src/modules/filemanager/components/OwnerStatusBar/OwnerStatusBar.tsx
+++ src/modules/filemanager/components/OwnerStatusBar/OwnerStatusBar.tsx
@@ -8,12 +8,18 @@ export function OwnerStatusBar(): ReactElement {
   return (
     <div className="fm-owner-status-bar-container">
       <div className="fm-owner-status-bar-left">
-        <InfoIcon />
         <div>Capacity: 8 GB/10 GB</div>
         <ProgressBar value={35} width={150} />
-        <div>Expiration: 2025-05-25</div>
+        <div>File Manager Available: Until: 2025-05-25</div>
+        <div className="fm-owner-status-bar-info-tooltip-wrapper">
+          <InfoIcon size="16px" />
+          <span className="fm-owner-status-bar-tooltip">
+            The File Manager works only while your storage remains valid. If it expires, all catalogue metadata is
+            permanently lost.
+          </span>
+        </div>
       </div>
-      <div className="fm-owner-status-bar-upgrade-button">Upgrade</div>
+      <div className="fm-owner-status-bar-upgrade-button">Manage</div>
     </div>
   )
 }
