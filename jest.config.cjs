3d641eb57605a6cc94310aa590c9a366d81b9ff7
Balint Ujvari <balint.ujvari@solarpunk.buzz>
2026-02-13 15:22:03 +0100

    fix: jest depcheck error

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  transformIgnorePatterns: ['node_modules/(?!(std-env|cafe-utility|bee-js)/)'], // TODO: review all test configs
+  transformIgnorePatterns: ['node_modules/(?!(std-env|cafe-utility|bee-js)/)'], // TODO: review all test configs and fix ui-test/
   extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   modulePathIgnorePatterns: ['<rootDir>/build/'],
